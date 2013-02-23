7e5a3f9370b601deca71c36bb64b5ea136d95b7a
ekloswic@nd.edu <ekloswic@nd.edu@8bb050b1-e52f-486a-bdb0-e2b5eea0feb1>
2013-02-23 17:16:49 +0000

    Added lab3

---
+++ lab3/robot_chocolate/build/CMakeFiles/robot_chocolate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/robot_chocolate.dir/nodes/robot_chocolate.o"
+  "../bin/robot_chocolate.pdb"
+  "../bin/robot_chocolate"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/robot_chocolate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

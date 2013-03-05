fe4694898bf8b9765859d9ae3208c841b23f568b
ekloswic@nd.edu <ekloswic@nd.edu@8bb050b1-e52f-486a-bdb0-e2b5eea0feb1>
2013-03-05 18:33:10 +0000

    added lab4

---
+++ trunk/lab4/robot_slalom/build/CMakeFiles/robot_slalom.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/robot_slalom.dir/nodes/robot_slalom.o"
+  "../bin/robot_slalom.pdb"
+  "../bin/robot_slalom"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/robot_slalom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
